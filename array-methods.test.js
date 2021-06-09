4f84382f0aa36df2f1c8b38ff527243680f1de7a
Tis Lais <tistististis@gmail.com>
2021-06-08 18:02:16 -0700

    add second map test for handling strings

--- array-methods.test.js
+++ array-methods.test.js
@@ -1,15 +1,26 @@
 import { map } from './array-methods.js';
 
-const arrayOne = [1, 2, 3, 4, 5];
 
 function multiplyByThree(number) {
   return number * 3;
 }
 
+function addBeans(string) {
+  return `${string} beans`;
+}
+
 describe('map', () => {
   it('multiplies each item of array by 3', () => {
+    const arrayOne = [1, 2, 3, 4, 5];
     const actual = map(arrayOne, multiplyByThree);
     const expected = [3, 6, 9, 12, 15];
+    expect(actual).toEqual(expected);
+  });
+  
+  it('takes a string and adds beans', () => {
+    const input = ['one', 'two', 'red', 'blue'];
+    const actual = map(input, addBeans);
+    const expected = ['one beans', 'two beans', 'red beans', 'blue beans'];
     expect(actual).toEqual(expected);
   });
 });
